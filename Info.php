b71c83b3f16675f1f8ed3b339c2946d2ba028354
DefinitelyNotDecrocro <mdnaim.mdnazri@student.gmi.edu.my>
2025-09-08 20:32:42 +0800

    Add files via upload

---
+++ Info.php
@@ -0,0 +1,22 @@
+<HTML>
+
+<head>
+<title>King Information</title>
+<h1>Maklumat pelajar</h1>
+</head>
+
+<body>
+<?php
+
+$Nama = 'Nazran The King';
+$ID = 'SM15684';
+$Hobby = 'Send Sticker Whatsapp';
+
+echo "<p><b> Name : </b> $Nama</p>";
+echo "<p><b> King ID : </b> $ID</p>";
+echo "<p><b> King Hobby : </b> $Hobby</p>";
+
+?>
+</body>
+
+</HTML>
